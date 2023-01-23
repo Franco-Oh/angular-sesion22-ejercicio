93a3d30865f71ff5cc9fb9220eb46551dbfb2b80
Franco Ojeda <fojedavideos@gmail.com>
2023-01-23 09:34:11 -0500

    iniciar ejercicio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular2023-66036',
+    appId: '1:573634161533:web:b533f8af8679e11175fa14',
+    storageBucket: 'angular2023-66036.appspot.com',
+    apiKey: 'AIzaSyBZgvvVbU_IC-ZZE_nlPFx5kLFGX_BKRYk',
+    authDomain: 'angular2023-66036.firebaseapp.com',
+    messagingSenderId: '573634161533',
+  },
   production: false
 };
 
